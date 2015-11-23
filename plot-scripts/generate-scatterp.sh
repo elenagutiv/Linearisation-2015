7cdbf95ef407bb93292dd4ee4f5a6784b177ba63
Elena Gutiérrez <elenagutiv@gmail.com>
2015-11-23 17:33:11 +0100

    Add generate scatterp.sh script to run mustache

---
+++ plot-scripts/generate-scatterp.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ $# != 2 ] || [ $1 != "-p" ] || ([ $2 != 1 ] && [ $2 != 2 ]); then
+	echo "Usage: ./generate-scatterplot.sh -p <plot_id> where"
+	echo "<plot-id> = 1 to generate elp-scatterplot-P1-P2"
+	echo "<plot-id> = 2 to generate elp-scatterplot-P0-PK"
+	echo ""
+	echo "This script requires mustache to be installed"
+	exit
+fi
+
+if [ $2 -eq 1 ]; then 
+	TEMPLATE="scatterplot-template-1.html"
+	PLOT="elp-scatterplot-P1-P2.html"
+else
+	TEMPLATE="scatterplot-template-2.html"
+	PLOT="elp-scatterplot-P0-PK.html"
+fi
+
+TRANSLATOR="translator.yml"
+
+# Run mustache:
+
+echo "Running mustache..."
+cat $TRANSLATOR | mustache - $TEMPLATE > $PLOT
+
+echo "HTML file: $PLOT"
