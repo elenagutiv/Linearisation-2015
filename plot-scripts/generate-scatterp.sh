f4bb970aaf12b127ea344d8e95c96c10de0e8567
Elena Gutiérrez <elenagutiv@gmail.com>
2016-02-29 18:43:38 +0100

    Add new python cript to compare ELP-PE. Add new files to generate new plot in directory plot-scripts/ and corresponding runtimes.

--- plot-scripts/generate-scatterp.sh
+++ plot-scripts/generate-scatterp.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if [ $# != 2 ] || [ $1 != "-p" ] || ([ $2 != 1 ] && [ $2 != 2 ]); then
+if [ $# != 2 ] || [ $1 != "-p" ] || ([ $2 != 1 ] && [ $2 != 2 ] && [ $2 != 3 ]); then
 	echo "Usage: ./generate-scatterplot.sh -p <plot_id> where"
 	echo "<plot-id> = 1 to generate elp-scatterplot-P1-P2"
 	echo "<plot-id> = 2 to generate elp-scatterplot-P0-PK"
+	echo "<plot-id> = 3 to generate elp-scatterplot-ELP-PE"
 	echo ""
 	echo "This script requires mustache to be installed"
 	exit
@@ -12,9 +13,12 @@ fi
 if [ $2 -eq 1 ]; then 
 	TEMPLATE="scatterplot-template-1.html"
 	PLOT="elp-scatterplot-P1-P2.html"
-else
+elif [ $2 -eq 2 ]; then 
 	TEMPLATE="scatterplot-template-2.html"
 	PLOT="elp-scatterplot-P0-PK.html"
+else
+	TEMPLATE="scatterplot-template-3.html"
+	PLOT="elp-scatterplot-ELP-PE.html"
 fi
 
 VIEW="running-times.yml"
